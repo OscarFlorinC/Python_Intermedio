b795a00b7b488c082d2311feddbd9185cd98452b
OscarFlocinC <oscar.florinc@icloud.com>
2024-04-19 18:32:20 -0600

    Update .py NI07

--- Python_NI/Python_NI07.py
+++ Python_NI/Python_NI07.py
@@ -1,15 +1,20 @@
 # Ejemplo herencia múltiple
 class A():
     def primera (self):
-        return "Esta es la clase A"
+        return "La clase A"
 
 class B():
     def segunda(self):
-        return "Esta es la clase B"
+        return "La clase B"
 
 class C(A, B):
     pass
 
+a = A()
+print(f"Esta es la clase: ", a.primera())
+b = B()
+print(f"Esta es la clase: ", b.segunda())
+
 c = C()
-print(c.primera())
-print(c.segunda())
+print(f"Esta es la clase C con herencia de: ", c.primera())
+print(f"Esta es la clase C con herencia de: ",c.segunda())
